ecee4939e84f9bf9577c7fb9172283ee44c3a926
K-Soo <kanabun102@naver.com>
2024-02-12 12:58:54 +0900

    Initial Commit

---
+++ webview-swift-javascript-bridge/webview_swift_javascript_bridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  webview_swift_javascript_bridgeApp.swift
+//  webview-swift-javascript-bridge
+//
+//  Created by admin on 2024/02/12.
+//
+
+import SwiftUI
+
+@main
+struct webview_swift_javascript_bridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
